68ca69efe466e27dc296b679b8810d4e66d2c15f
kazuki <s1250040a@gmail.com>
2020-05-12 21:36:41 +0900

    fixed

--- server/server.go
+++ server/server.go
@@ -3,21 +3,21 @@ package server
 import (
 	"github.com/gin-gonic/gin"
 
-	user "github.com/s1250040/go-yumemori/controller"
+	"github.com/s1250040/go-yumemori/controller/user_controller"
 )
 
 // Init is initialize server
 func Init() {
 	r := router()
-	r.Run()
+	r.Run() //サーバーを起動
 }
 
 func router() *gin.Engine {
-	r := gin.Default()
+	r := gin.Default() //ginの変数を定義
 
 	u := r.Group("/users")
 	{
-		ctrl := user.Controller{}
+		ctrl := user_controller.Controller{}
 		u.GET("", ctrl.Index)
 		u.GET("/:id", ctrl.Show)
 		u.POST("", ctrl.Create)
