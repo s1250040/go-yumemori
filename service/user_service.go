912f882ac80ca0887f980668c7cd1cbecc0a6f60
kazuki <s1250040a@gmail.com>
2020-08-14 14:46:05 +0900

    test

--- service/user_service.go
+++ service/user_service.go
@@ -1,8 +1,6 @@
 package user
 
 import (
-	"time"
-
 	"github.com/gin-gonic/gin"
 
 	"github.com/s1250040/go-yumemori/db"
@@ -17,7 +15,7 @@ type User entity.User
 
 // あとでuser.goに追加
 type Result struct {
-	sampling_start_time time.Time
+	sampling_start_time int
 }
 
 // GetAll is get all User
